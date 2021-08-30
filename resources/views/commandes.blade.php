1157aa487480505db9818cf83f3be5dcd4089f05
Othhmane <73822774+Othhmane@users.noreply.github.com>
2021-08-31 00:49:24 +0100

    dddd

--- resources/views/commandes.blade.php
+++ resources/views/commandes.blade.php
@@ -36,10 +36,7 @@
             <td>{{$order->city}}</td>
             <td><a href="{{ route('pdfTest', $order->id) }}" class="btn btn-xs btn-info pull-right">Imprimer</a></td>
 
-            <td><a data-toggle="modal" id="smallButton" data-target="#smallModal"
-            data-attr="{{ route('show', $order->id) }}" title="show">
-             <i class="fas fa-eye text-success  fa-lg"></i>
-             </a></td>
+
           </tr>
         </tbody>
         @endforeach
@@ -48,79 +45,10 @@
 </div>
 </div>
 </div>
-    <!-- small modal -->
-    <div class="modal fade" id="smallModal" tabindex="-1" role="dialog" aria-labelledby="smallModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog modal-sm" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body" id="smallBody">
-                    <div>
-                        <!-- the result to be displayed apply here -->
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+   
 
 
-    <script>
-        // display a modal (small modal)
-        $(document).on('click', '#smallButton', function(event) {
-            event.preventDefault();
-            let href = $(this).attr('data-attr');
-            $.ajax({
-                url: href,
-                beforeSend: function() {
-                    $('#loader').show();
-                },
-                // return the result
-                success: function(result) {
-                    $('#smallModal').modal("show");
-                    $('#smallBody').html(result).show();
-                },
-                complete: function() {
-                    $('#loader').hide();
-                },
-                error: function(jqXHR, testStatus, error) {
-                    console.log(error);
-                    alert("Page " + href + " cannot open. Error:" + error);
-                    $('#loader').hide();
-                },
-                timeout: 8000
-            })
-        });
+  
 
-        // display a modal (medium modal)
-        $(document).on('click', '#mediumButton', function(event) {
-            event.preventDefault();
-            let href = $(this).attr('data-attr');
-            $.ajax({
-                url: href,
-                beforeSend: function() {
-                    $('#loader').show();
-                },
-                // return the result
-                success: function(result) {
-                    $('#mediumModal').modal("show");
-                    $('#mediumBody').html(result).show();
-                },
-                complete: function() {
-                    $('#loader').hide();
-                },
-                error: function(jqXHR, testStatus, error) {
-                    console.log(error);
-                    alert("Page " + href + " cannot open. Error:" + error);
-                    $('#loader').hide();
-                },
-                timeout: 8000
-            })
-        });
-
-    </script>
 
 </x-app-layout>
